03bc37cda8b51bf1f478b818c1d500871a67a8a2
Przemo <przemo06133@gmail.com>
2023-05-10 19:54:23 +0200

    Adjust Welcome page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { TransProvider } from './context/TransContext';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <TransProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </TransProvider>
+
 )
---
+++ client/src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+export const contractAddress = '0xdBFb8e4Bb75493b00fb4a29B467c0a53Ee35F63D'
+
+
